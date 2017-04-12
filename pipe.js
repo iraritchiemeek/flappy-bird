5c2a8d4f91789c1999d9bb5848507bc2a2890a24
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-13 09:56:40 +1200

    add collision detection

--- pipe.js
+++ pipe.js
@@ -5,8 +5,13 @@ function Pipe() {
 	this.w = 20
 	this.speed = 2
 
+	this.highlight = false
+
 	this.show = function () {
 		fill(255)
+		if (this.highlight) {
+			fill(255, 0, 0)
+		}
 		rect(this.x, 0, this.w, this.top)
 		rect(this.x, height-this.bottom, this.w, this.bottom)
 	}
@@ -21,5 +26,16 @@ function Pipe() {
 		} else {
 			return false
 		}
+	}
+
+	this.hits = function (bird) {
+		if (bird.y < this.top || bird.y > height - this.bottom) {
+			if (bird.x > this.x && bird.x < this.x + this.w) {
+				this.highlight = true
+				return true
+			}
+		}
+	this.highlight = false
+	return false
 	}
 }
--- sketch.js
+++ sketch.js
@@ -8,17 +8,23 @@ function setup() {
 }
 
 function draw() {
-	background(0) 
-	bird.show()
-	bird.update()
+	background(0)
 
 	for (var i = pipes.length-1; i >= 0; i--) {
 		pipes[i].show()
 		pipes[i].update()
+
+		if (pipes[i].hits(bird)) {
+			console.log('hit')
+		}
+
 		if (pipes[i].offscreen()) {
 			pipes.splice(i, 1)
 		}
 	}
+
+	bird.show()
+	bird.update()
 
 	if (frameCount % 100 == 0) {
 		pipes.push(new Pipe())

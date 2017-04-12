48cff93091bee22c5facc367356d28293a76537f
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-13 09:46:01 +1200

    add pipes

--- bird.js
+++ bird.js
@@ -1,8 +1,8 @@
 function Bird() {
 	this.y = height/2
 	this.x = 64
-	this.gravity = 0.1
-	this.lift = -10
+	this.gravity = 0.6
+	this.lift = -15
 	this.velocity = 0
 
 	this.show = function () {
@@ -16,6 +16,7 @@ function Bird() {
 
 	this.update = function () {
 		this.velocity += this.gravity
+		this.velocity *=0.9
 		this.y += this.velocity
 
 		if(this.y > height){
--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
 var bird
+var pipes = []
 
 function setup() {
 	createCanvas(400, 600)
 	bird = new Bird()
+	pipes.push(new Pipe())
 }
 
 function draw() {
 	background(0) 
 	bird.show()
 	bird.update()
+
+	for (var i = pipes.length-1; i >= 0; i--) {
+		pipes[i].show()
+		pipes[i].update()
+		if (pipes[i].offscreen()) {
+			pipes.splice(i, 1)
+		}
+	}
+
+	if (frameCount % 100 == 0) {
+		pipes.push(new Pipe())
+	}
 }
 
 function keyPressed() {
